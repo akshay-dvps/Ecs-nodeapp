a68db01485a1eb9cd4a6a99231fd0ac9713cf01e
akshay-dvps <136419934+akshay-dvps@users.noreply.github.com>
2023-07-08 21:57:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('Sample node applicationn!');
+  response.end('Sample node applicationn mumbai!');
   
 };
 var www = http.createServer(handleRequest);
